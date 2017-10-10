2572984e1e2cf24f0552086aa3be34d3c7448897
RebelBird <chemwenxin@163.com>
2017-10-10 14:33:15 +0800

    test.py

--- blogproject/fabfile.py
+++ blogproject/fabfile.py
@@ -11,13 +11,14 @@ env.hosts = ['119.23.221.255']
 env.port = '22'
 
 def deploy():
-	source_folder = 'home/rebelbird/sites/119.23.221.255/Blog'
+	source_folder = '/home/rebelbird/sites/119.23.221.255/Blog'
 
 	run('cd %s && git pull' % source_folder)
 	run("""
 		cd {} &&
 		../env/bin/pip install -r requirements.txt &&
-		../env/bin/python3 manage.py collectsatic --noinput &&
+		../env/bin/python3 manage.py collectstatic --noinput &&
+		../env/bin/python3 manage.py makemigrations &&
 		../env/bin/python3 manage.py migrate
 		""".format(source_folder))
 	sudo('restart gunicorn-119.23.221.255')
